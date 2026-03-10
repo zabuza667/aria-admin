558aca681bc1b5d04eafda0123cf360d3b3b7a5d
zabuza667 <zabuza667@outlook.com>
2026-03-10 21:52:48 +0100

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  root: '.',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: './index.html'
+    }
+  },
+  server: { port: 5173 }
+})
